eb17a3e4e071197f3cb6bae6578bdd938c339ced
Bagas <mbagas221@gmail.com>
2022-10-21 14:36:02 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from "./components/home";
+import Detail from "./components/detail";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/detail" element={<Detail />}/>
       </Routes>
     </Router>
   );
